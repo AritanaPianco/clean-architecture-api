d2829d3220b08f597df424d13f32e8fef33c6bb3
AritanaPianco <aritana.pianco@gmail.com>
2024-12-20 21:14:18 -0300

    chore: adding new jest scripts to run

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+// import type { Config } from 'jest'
+
+module.exports = {
+  roots: ['<rootDir>/src/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  testEnvironment: 'jest-environment-node'
+
+}
+// export default config
