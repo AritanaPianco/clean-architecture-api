1811d2a9dee93f9a88846f66fb5c620a460951be
AritanaPianco <aritana.pianco@gmail.com>
2024-12-22 15:14:40 -0300

    test: ensure Bcrypt returns a hashedValue on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,6 +2,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hashedValue'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct value', async () => {
     const salt = 8
@@ -9,5 +15,11 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('should return a hash on success', async () => {
+    const salt = 8
+    const sut = new BcryptAdapter(salt)
+    const hashedValue = await sut.encrypt('any_value')
+    expect(hashedValue).toBe('hashedValue')
   })
 })
