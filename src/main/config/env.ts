b6c6277864c93680a977b727b1f1256323d71382
AritanaPianco <aritanapianco10@gmail.com>
2025-01-31 16:49:06 -0300

    chore: add docker compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '0hv8z1gfw4a8gfgoyb8jb2ccsr32ham715yahy2zp36k7d0298uhdoquq3c2lj1c'
 }
