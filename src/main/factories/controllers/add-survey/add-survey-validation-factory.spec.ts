aa60d9dcc0f8096c4c73c944dd1ce0201aa3fe2a
AritanaPianco <aritanapianco10@gmail.com>
2025-02-07 19:48:59 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { type Validation } from '../../../../presentation/protocols/validation'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('SurveyValidation Factory', () => {
+  test('should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    const validations: Validation[] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+
+let surveyCollection: Collection
+describe('Surveys Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL!)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('should return 204 on add survey', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      await request(app)
+        .post('/api/survey').send({
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
